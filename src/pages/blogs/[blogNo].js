f519b82292820adda5ae3aefa6d6632841726b5d
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-03-20 15:34:13 +0530

    23 getStaticPaths

--- src/pages/blogs/[blogNo].js
+++ src/pages/blogs/[blogNo].js
@@ -1,11 +1,49 @@
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 
-const BlogNo = () => {
-    const router = useRouter();
-    const pageNo = router.query.blogNo
+export const getStaticPaths = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const data = await res.json();
+
+    const paths = data.map((currElem) => {
+        return {
+            params: {
+                blogNo: currElem.id.toString(),
+            },
+        }
+    });
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async (context) => {
+    const id = context.params.blogNo;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    const data = await res.json()
+
+    return {
+        props: {
+            data,
+        }
+    }
+}
+
+const BlogNo = ({ data }) => {
+    // const router = useRouter();
+    // const pageNo = router.query.blogNo
+
+    const { id, title, body } = data
     return (
         <>
-            <h1>Blog {pageNo} content here</h1>
+            {/* <h1>Blog {pageNo} content here</h1> */}
+
+            <div className="ssr-styles ssr-styles-inside">
+                <h3>{id}</h3>
+                <h2>{title}</h2>
+                <p>{body}</p>
+            </div>
+
         </>
     )
 }
--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Link from 'next/link'
 
 // fetch data at build time
 export const getStaticProps = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts/")
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
     const data = await res.json()
 
     return {
@@ -15,10 +16,10 @@ const index = ({ data }) => {
     return (
         <>
             {
-                data.map((currElem) => {
+                data.slice(0, 5).map((currElem) => {
                     return <div key={currElem.id} className="ssr-styles">
                         <h3>{currElem.id}</h3>
-                        <h2>{currElem.title}</h2>
+                        <Link href={`/blogs/${currElem.id}`}><h2>{currElem.title}</h2></Link>
                     </div>
                 })
             }
